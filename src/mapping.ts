a4f2909965aef46baef48438e341d08351922443
NoxApi <pcsnon@gmail.com>
2022-04-24 17:42:36 +0700

    v1

--- src/mapping.ts
+++ src/mapping.ts
@@ -13,94 +13,41 @@ import {
   Transfer,
   UpdateLockChoice
 } from "../generated/scbmumbai/scbmumbai"
-import { ExampleEntity } from "../generated/schema"
+import { user,token,foodlist } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.MAX_SUPPLY(...)
-  // - contract.PRICE_PER_UNIT(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getRewardForDuration(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.lastTimeRewardApplicable(...)
-  // - contract.lockChoices(...)
-  // - contract.lockedSupply(...)
-  // - contract.name(...)
-  // - contract.nft(...)
-  // - contract.nftLocks(...)
-  // - contract.nftRewardPerTokenPaid(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.rewardData(...)
-  // - contract.rewardDistributors(...)
-  // - contract.rewardPerToken(...)
-  // - contract.rewardTokens(...)
-  // - contract.rewards(...)
-  // - contract.rewardsDuration(...)
-  // - contract.seller(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalERC20Supply(...)
-  // - contract.totalSupply(...)
+export function handleFeed(event: Feed): void {
+  // let entity = food.load(event.params.tokenId.toString())
+  // if (entity== null) {
+  //   entity = new food(event.params.tokenId.toString())
+  // }
+  // let foods = new foodlist(event.params.unlockTime.toString())
+  // foods.amount = event.params.amount
+  // foods.TokenID = event.params.tokenId
+  // foods.isclaim = false
+}
+export function handleReclaim(event: Reclaim): void {
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+//export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleEvole(event: Evole): void {}
 
-export function handleFeed(event: Feed): void {}
+export function handleMint(event: Mint): void {
+  let ID = new token(event.params.tokenId.toString())
+  ID.evolform = new BigInt(0)
+  ID.race = new BigInt(0)
+  ID.amount =new BigInt(0)
+  ID.User=event.params.user
+  ID.save()
+}
 
-export function handleMint(event: Mint): void {}
+//export function handleNewLockChoice(event: NewLockChoice): void {}
 
-export function handleNewLockChoice(event: NewLockChoice): void {}
+//export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleReclaim(event: Reclaim): void {}
+//export function handleRewardPaid(event: RewardPaid): void {}
 
-export function handleRewardPaid(event: RewardPaid): void {}
+//export function handleTransfer(event: Transfer): void {}
 
-export function handleTransfer(event: Transfer): void {}
-
-export function handleUpdateLockChoice(event: UpdateLockChoice): void {}
+//export function handleUpdateLockChoice(event: UpdateLockChoice): void {}
